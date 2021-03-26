7c60ba1c2092c3f091dfd9078a5d25b8da18aee2
root <root@oklolindevelop.oklonet.local>
2021-03-26 15:52:47 +0100

    Moved handling of program phases to separate file

---
+++ programphases.php
@@ -0,0 +1,71 @@
+<?php
+return array(
+    // Washing Machine
+    "256" => "Not running",
+    "257" => "Pre-Wash",
+    "258" => "Soak",
+    "259" => "Pre-Wash",
+    "260" => "Main Wash",
+    "261" => "Rinse",
+    "262" => "Rinse Hold",
+    "263" => "Main Wash",
+    "264" => "Cooling down",
+    "265" => "Drain",
+    "266" => "Spin",
+    "267" => "Anti-crease",
+    "268" => "Finished",
+    "269" => "Venting",
+    "270" => "Starch Stop",
+    "271" => "Freshen-up + Moisten",
+    "272" => "Steam Smoothing",
+    "279" => "Hygiene",
+    "280" => "Drying",
+    "285" => "Disinfection",
+    "295" => "Steam Smoothing",
+
+    // Clothes dryer
+    "512" => "Not running",
+    "513" => "Program Running",
+    "514" => "Drying",
+    "515" => "Machine Iron",
+    "516" => "Hand Iron",
+    "517" => "Normal",
+    "518" => "Normal Plus",
+    "519" => "Cooling down",
+    "520" => "Hand Iron",
+    "521" => "Anti-crease",
+    "522" => "Finished",
+    "523" => "Extra Dry",
+    "524" => "Hand Iron",
+    "526" => "Moisten",
+    "528" => "Timed Drying",
+    "529" => "Warm Air",
+    "530" => "Steam Smoothing",
+    "531" => "Comfort Cooling",
+    "532" => "Rinse out lint",
+    "533" => "Rinses",
+    "534" => "Smoothing",
+    "538" => "Slightly Dry",
+    "539" => "Safety Cooling",
+
+    // Dish washer
+    "1792" => "Not running",
+    "1793" => "Reactivating",
+    "1794" => "Pre-wash",
+    "1795" => "Main wash",
+    "1796" => "Rinse",
+    "1797" => "Interim Rinse",
+    "1798" => "Final rinse",
+    "1799" => "Drying",
+    "1800" => "Finished",
+    "1801" => "Pre-Wash",
+    "Default" => "Unknown: "
+)
+?>
+
+//$programPhaseRaw=$appliance['state']['programPhase']['value_raw'];
+			//if (isset($programPhase[$programPhaseRaw])) {
+			//	$programPhaseStr=$programPhase[$programPhaseRaw];
+			//} else {
+			//	$programPhaseStr=$programPhase["Default"] . $programPhaseRaw;
+			//}
